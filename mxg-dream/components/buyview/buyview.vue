f82ae1f9b5e5e2276843d1683116c6c4f853f941
Your Name <you@example.com>
2022-09-25 21:25:03 +0800

    刘征

---
+++ mxg-dream/components/buyview/buyview.vue
@@ -0,0 +1,31 @@
+<template>
+	<view>
+		<button>立即购买</button>
+	</view>
+</template>
+
+<script>
+	export default {
+		name:"buyview",
+		data() {
+			return {
+				
+			};
+		}
+	}
+</script>
+
+<style lang="scss">
+	button{
+		box-sizing: border-box;
+		position: fixed;
+		bottom: 0rpx;
+		left: 0;
+		width: 100%;
+		background-color: #345dc2;
+		padding: 0  30rpx;
+		text-align: center;
+		border-radius: 30px;
+		color: #fff;
+	}
+</style>
